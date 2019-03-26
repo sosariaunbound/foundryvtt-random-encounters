0b499a751226300882b29cdde4af1c28d76037ca
Zach <zach@wildfigdata.com>
2019-03-25 22:52:06 -0400

    Working popup window

--- encounter-creator.js
+++ encounter-creator.js
@@ -9,29 +9,30 @@ class EncounterCreator extends Application {
         super(app);
         this.hookActorDirectory();
     }
+
+    static get defaultOptions() {
+        const options = super.defaultOptions;
+        options.id = "track-encounter-creator-form";
+        options.template = "public/modules/encounter-creator/templates/encounter-creator.html";
+        options.width = 360;
+        return options;
+    }
+
     hookActorDirectory() {
         Hooks.on('renderActorDirectory', (app, html, data) => {
 
-            const importButton = $(`<button class="encounter-creator-btn"><i class="fas fa-fire"></i> Generate Encounter</button>`);
+            const encounterCreatorButton = $(`<button class="encounter-creator-btn"><i class="fas fa-fire"></i> Generate Encounter</button>`);
 
             if (game.user.isGM) {
-                html.find('.directory-footer').prepend(importButton);
+                html.find('.directory-footer').prepend(encounterCreatorButton);
             }
+
+            encounterCreatorButton.click(ev => {
+                ev.preventDefault();
+                this.render(true);
+            });
         });
     }
 }
-
-
-
-// hookActorDirectory() {
-//     Hooks.on('renderActorDirectory', (app, html, data) => {
-//         const importButton = $(`<button class="encounter-creator-btn"><i class="fas fa-fire"></i> Encounter Creator</button>`);
-//         // slight diffrence in used buttons and layout depending on gm status
-
-//         if (game.user.isGM) {
-//             html.find('.directory-footer').append(importButton);
-//         }
-//     })
-// }
 
 let encounterCreator = new EncounterCreator();
